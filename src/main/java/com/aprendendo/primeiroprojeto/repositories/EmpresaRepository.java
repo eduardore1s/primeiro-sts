30a439804d3f41f8bf4c06ba977459d8ad1554d2
Eduardo Silveira dos Reis <bi009816@bhz-m-000416.intermedium.local>
2018-08-13 15:52:03 -0300

    inclusao de testes

---
+++ src/main/java/com/aprendendo/primeiroprojeto/repositories/EmpresaRepository.java
@@ -0,0 +1,12 @@
+package com.aprendendo.primeiroprojeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aprendendo.primeiroprojeto.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa FindByCnpj(String cnpf);
+}
--- src/test/java/com/aprendendo/primeiroprojeto/api/PrimeiroProjetoApplicationTests.java
+++ src/test/java/com/aprendendo/primeiroprojeto/api/PrimeiroProjetoApplicationTests.java
@@ -3,10 +3,12 @@ package com.aprendendo.primeiroprojeto.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class PrimeiroProjetoApplicationTests {
 
 	@Test
@@ -14,3 +16,4 @@ public class PrimeiroProjetoApplicationTests {
 	}
 
 }
+
