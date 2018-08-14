06f66c4fd6ebd4bb5aa023471168e3d65edd98ed
Eduardo Silveira dos Reis <bi009816@bhz-m-000416.intermedium.local>
2018-08-14 11:57:31 -0300

    Adicionado services

---
+++ src/main/java/com/aprendendo/primeiroprojeto/services/impl/EmpresaServiceImpl.java
@@ -0,0 +1,34 @@
+package com.aprendendo.primeiroprojeto.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aprendendo.primeiroprojeto.entities.Empresa;
+import com.aprendendo.primeiroprojeto.repositories.EmpresaRepository;
+import com.aprendndo.primeiroprojeto.services.EmpresaService;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+	private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	@Override
+	public Optional<Empresa> buscarPorCnpj(String cnpj) {
+		log.info("Buscando uma empresa para o CNPJ {}", cnpj);
+		return Optional.ofNullable(empresaRepository.FindByCnpj(cnpj));
+	}
+
+	@Override
+	public Empresa persistir(Empresa empresa) {
+		log.info("Persistindo empresa: {}", empresa);
+		return this.empresaRepository.save(empresa);
+	}
+
+}
--- src/test/java/com/aprendendo/primeiroprojeto/repositories/EmpresaRepositoryTest.java
+++ src/test/java/com/aprendendo/primeiroprojeto/repositories/EmpresaRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,12 +15,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.aprendendo.primeiroprojeto.entities.Empresa;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 @ActiveProfiles("test")
 public class EmpresaRepositoryTest {
 	
-	@Autowired 
+	@InjectMocks 
 	private EmpresaRepository empresaRepository;
 	
 	private static final String CNPJ = "51463645000100";
